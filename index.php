db1f4e2d0944aa94627792f1c6f0d5c6a731ee87
Zმհɾმ Rმzzმghi <47444155+ZahraRazzaghi@users.noreply.github.com>
2020-08-16 15:09:28 +0430

    add index.php
    add the site home page

---
+++ index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="X-UA-Compatible" content="ie=edge">
+    <title>fastscroll-learn programing fast</title>
+    <!---style & script files--->
+    <link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/all.min.css" rel="stylesheet">
+    <link href="assets/css/style_index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<!--- Start Header --->
+<!-- Responsive Menu -->
+<!--- Start Header --->
+
+<h1 id="fastscroll"></h1>
+
+<header class="index-showcase">
+    <div class="container">
+        
+        <nav>
+            <div class="navbar-brand">
+                <a href="index.php"> <i style="color: pink">fastscroll</i><img src="assets/images/logo-index.png" alt="logo" id="logo"></a>
+            </div>
+            <div class="menu-icons">
+                <i class="icon ion-md-menu"></i>
+            </div>
+            <ul class="nav-list">
+
+                <li class="nav-item">
+                    <a aria-current="page" class="nav-link current" href="AboutUs.php">درباره ما</a>
+                </li>
+                <li class="nav-item">
+                    <a aria-current="page" class="nav-link current" href="ContactUs.php">تماس با ما</a>
+                </li>
+                <li class="nav-item">
+                    <a aria-current="page" class="nav-link current" href="CommonQuestions.php">سوالات متداول</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="index-buttons">
+            <a href="SignUp.php" class="btn btn-outline-primary">ثبت نام<i class="fa fa-registered"></i></a>
+            <a href="SignIn.php" class="btn btn-outline-primary">ورود<i class="fa fa-sign-in"></i></a>
+        </div>
+    </div>
+</header>
+
+<!--- End Header --->
+
+<section class="hero">
+    <div class="container">
+        <div class="main-message">
+            <h3>
+                <br>
+                <div class="title">
+                    حرفه ای و رایگان یاد بگیرید!
+                </div>
+                <h1>FASTSCROLL.COM</h1>
+                <div class="text">  ما در این سایت جدیدترین و کاربردی ترین علم روز در زمینه برنامه نوسی را در اختیار شما قرار خواهیم داد.</div>
+                <br>
+                <div class="cta">
+                    <a href="Courses.php" class="btn btn-start-learning">شروع یادگیری</a>
+                </div>
+
+        </div>
+
+    </div>
+</section>
+
+<script src="assets/js/index-search-box.js"></script>
+
+<div class="wrapper">
+    <div class="search_box">
+        <div class="search_btn"><img src="assets/images/icons8-search-32.png" style="width: 2.5rem"></div>
+        <input type="text" class="input_search" placeholder="جستجو در میان آموزش ها">
+    </div>
+</div>
+<!--- End Header --->
+
+<!-- Start Services -->
+<main>
+        <!--Start Courses -->
+    <section class="experience-outdoors">
+        <div class="container">
+            <div class="title-heading">
+                <h3>Experience</h3>
+                <h2>The thrilling outdoors</h2>
+                <p>از 140 موسسه به 2500+ دوره آنلاین دسترسی پیدا کنید. امروز شروع کنید! </p>
+            </div>
+            <!--Start Courses -->
+            <section class="courses img-thumbnail">
+                <div class="container">
+                    <li class="nav-item">
+                        <a aria-current="page" class="nav-link current" href="ContactUs.php">آموزش ها</a>
+                        <ul>وب
+
+                            <li><a href="#">html</a> </li>
+                            <li><a href="#">css</a> </li>
+                        </ul>
+                    </li><a href="Courses.php" class="btn btn-outline-dark ">بیشتر</a>
+
+                </div>
+            </section>
+        </div>
+    </section>
+    <!--End Courses -->
+
+    <section class="testimonials">
+        <div class="container">
+            <div class="testimonial">
+                <div class="testimonial-text-box">
+                    <h3>زمان سرمایه گذاری روی خودتان است. </h3>
+                    <h4>از آنجا که یادگیری کد نویسی ممکن است ساده ترین راه برای تغییر شغل شما باشد.</h4>
+                    <i class="icon ion-md-quote"></i>
+                </div>
+                <a href="#" class="btn btn-outline-primary btn-start-learn2">همین الان یادگیری را شروع کنید</a>
+
+            </div>
+        </div>
+    </section>
+    <!-- -->
+
+    <section class="begin-adventure">
+        <div class="container2">
+            <div class="row">
+            <div class="title-heading col-md-2-5 img-thumbnail ">
+                <div class="article_number btn-light"><b>1</b></div>
+                <div class="article-text">
+                    <h3>چیزی را انتخاب کنید که می خواهید یاد بگیرید.</h3>
+                    <img src="assets/images/index-001.webp">
+                    <p>از ساخت وب سایت تا تحلیل داده ها ، انتخاب مورد نظر شماست. مطمئن نیستید از کجا شروع کنید؟ ما شما را در جهت درست هدایت خواهیم کرد. </p>
+                </div>
+                </div>
+            <div class="title-heading col-md-2-5 img-thumbnail">
+                <div class="article_number btn-light"><b>2</b></div>
+                <div class="article-text">
+                <h3>با انجام یادگیری</h3>
+                    <img src="assets/images/index-002.webp">
+                    <p>سطح تجربه شما مهم نیست ، شما برای چند دقیقه کد واقعی را می نویسید.</p>
+                    </div>
+            </div>
+            <div class="title-heading col-md-2-5 img-thumbnail">
+                <div class="article_number btn-light"><b>3</b></div>
+                <div class="article-text">
+                    <h3>بازخورد فوری دریافت کنید</h3>
+                    <img src="assets/images/index-003.webp">
+                    <p> کد شما به محض ارسال آن تست می شود ، بنابراین همیشه می دانید که آیا در مسیر صحیحی هستید. </p>
+                </div>
+            </div>
+            <div class="title-heading col-md-2-5 img-thumbnail">
+                <div class="article_number btn-light"><b>4</b></div>
+                <div class="article-text">
+                    <h3>یادگیری را عملی تجربه کنید.</h3>
+                    <img src="assets/images/index-004.webp">
+                    <p>یادگیری خود را روی پروژه های دنیای واقعی اعمال کنید و دانش خود را با آزمون های متناسب امتحان کنید. </p>
+                </div>
+            </div>
+            <div class="title-heading col-md-2-5 img-thumbnail">
+                <div class="article_number btn-light"><b>5</b></div>
+                <div class="article-text">
+                    <h3>شغل رویایی خود را شروع کنید</h3>
+                    <img src="assets/images/index-005.webp">
+                    <p>مهارت های برنامه نویسی هرگز مورد تقاضای بیشتر نبوده است. همه چیز را برای رسیدن به حرفه خود به سطح بعدی بیاموزید. </p>
+                </div>
+            </div></div>
+        </div>
+    </section>
+</main>
+
+<section class="center">
+    <div class="container grid-3">
+        <div class="col-md-4">
+            <i class="" style="color:#8ab1f0;"></i>
+            <h3>توسعه وب</h3>
+            <div class="row">
+                <p class="col-md-6 btn">HTML/CSS</p>
+                <p class="col-md-6 btn">JAVASCRIPT</p>
+            </div>
+            <p>fgh</p>
+
+        </div>
+        <div class="col-md-4">
+            <i class="" style="color:#8ab1f0;"></i>
+            <h3>برنامه نویسی و علوم کامپیوتر</h3>
+            <div class="row">
+                <p class="col-md-6">PYTHON</p>
+                <p class="col-md-6">CMD LINE</p>
+            </div>
+            <p>fgfg</p>
+
+        </div>
+
+        <div class="col-md-4">
+            <i class="" style="color:#8ab1f0;"></i>
+            <h3>علوم داده</h3>
+            <div class="row">
+                <p class="col-md-6">PYTHON</p>
+                <p class="col-md-6">SQL</p>
+            </div>
+            <p>fghfg</p>
+
+        </div>
+
+    </div>
+</section>
+<!-- End Services -->
+<section class="index-services center">
+    <div class="container grid-3">
+
+        <i class="fab fa-youtube fa-3x" style="color:#8ab1f0;"></i>
+        <h3>YouTube</h3>
+        <p>services text</p>
+
+        <i class="fas fa-chalkboard-teacher fa-3x" style="color:#8ab1f0;"></i>
+        <h3>آموزش ها</h3>
+        <p>services text</p>
+
+        <i class="fas fa-briefcase fa-3x" style="color:#8ab1f0;"></i>
+        <h3>Freelancer Projects</h3>
+        <p>services text</p>
+
+    </div>
+</section>
+
+<!-- About -->
+<section class="index-about bg-light">
+    <div class="container">
+        <div class="grid-2">
+            <div class="center">
+                <i class="fas fa-laptop-code fa-10x" style="color:#8ab1f0;"></i>
+            </div>
+            <div>
+                <h3>درباره ما</h3>
+                <p>some text</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--- Start footer --->
+<div id="footer">
+    <p class="text-for-socials">ما را در شبکه های اجتماعی دنبال کنید</p>
+    <div class="footerContent wrapper">
+        <div class="socialCounts row social-img-group">
+
+            <a class="col-md-3" href="https://twitter.com/FastScroll/" target="_blank">
+                <div ><img src="assets/images/Social-img/twitter.png" class="social-img" title="twitter"></div>
+            </a>
+            <a class="col-md-3" href="http://www.facebook.com/FastScroll/" target="_blank">
+                <div class=""><img src="assets/images/Social-img/facebook.png" class="social-img" title="facebook"></div>
+            </a>
+            <a class="col-md-3" href="http://www.github.com/FastScroll/" target="_blank">
+                <div class=""><img src="assets/images/Social-img/github.png" class="social-img" title="Github"></div>
+            </a>
+            <a class="col-md-3" href="http://www.linkdin.com/FastScroll/" target="_blank">
+                <div class=""><img src="assets/images/Social-img/linkedin.png" class="social-img" title="Linkdin"></div>
+            </a>
+        </div>
+        <div class="about">
+            <div class="logoFooter"></div>
+            <div>
+                <p dir="ltr">FastScroll Inc.</p>
+                <address dir="ltr">
+                    <br>4 Embarcadero Center, Suite 2020
+                    <br>iran, CA 94111</address>
+            </div>
+            <button  title="Contact Us" >برای ما ایمیل ارسال کنید</button>
+        </div>
+        <div class="footerMenu Courses">
+            <ul>
+                <li>
+                    <a href="index.php" id="footerHome">خانه</a>
+                </li>
+                <li>
+                    <a href="AboutUs.php">درباره ما</a>
+                </li>
+                <li>
+                    <a href="ContactUs.php" id="footerCourses">ارتباط با ما</a>
+                </li>
+                <li>
+                    <a href="CommonQuestions.php" id="footerBlog">سوالات متداول</a>
+                </li>
+                <li>
+                    <a href="Courses.php" id="footerContacts">آموزش ها</a>
+                </li>
+                <li>
+                    <a href="#" id="footerPrivacy-Policy">Terms of Use</a>
+                </li>
+                <li>
+                    <a href="#" id="faq" >FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <a href="#fastscroll" class="anchor" title="بالا"><img src="assets/images/sort-up-64.png" width="5%" alt="بالا"></a>
+    <div class="copyright" dir="ltr">
+        © 2020 FastScroll, Inc. All rights reserved.
+    </div>
+</div>
+<!---End footer --->
+<!--
+<script>
+    /* function expression to select elements*/
+    const selectElement=(s)=>document.querySelector(s);
+    //OPEN THE MENU ONCLICK
+    selectElement( s:'.open').()=>{
+        selectElement(s:'nav-list').classList.add('active');
+    });
+    //CLOSE THE MENU ONCLICK
+    selectElement( s:'.close').addEventListener()=>{
+        selectElement(s:'nav-list')remove.add(tokens:'active');
+    });
+</script>
+-->
+</body>
+</html>
