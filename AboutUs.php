d8af594ae432e5857a378d1549c8cfd853894048
Zმհɾმ Rმzzმghi <47444155+ZahraRazzaghi@users.noreply.github.com>
2020-08-31 10:33:04 +0430

    last changes on this file Complete

--- AboutUs.php
+++ AboutUs.php
@@ -4,83 +4,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="X-UA-Compatible" content="ie=edge">
-    <title>about us</title>
+    <meta name="keywords" content="Courses, HTML, CSS, JavaScript, C++, PHP, SQL, Java, Python, Ruby, C#, JQuery, Swift, Coding, Code, Programming,about us,درباره ما,برنامه نویسی,آموزش برنامه نویسی">
+    <title>About Us-FastScroll.com</title>
     <link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet">
     <link href="assets/css/style-AboutUs.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/style-top-menu.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 <!-- Ankhor -->
 <h1 id="fastscroll"></h1>
-<!--Header-->
+<!--Start Header-->
 <header id="cabecalho">
     <a href="#" id="logo"><img src="assets/images/logo.png" style="width: 3rem;height: auto"><i>fastscroll</i></a>
     <nav dir="rtl">
         <a href="#" id="menu-icon"><img src="assets/images/menu-icon.png" style="width: 3rem;height: auto"> </a>
         <ul>
-            <li><a href="index.php" > خانه</a></li>
-            <li><a href="AboutUs.php">درباره ما</a></li>
-            <li><a href="ContactUs.php">تماس با ما</a></li>
-            <li><a href="forum/controllers/Forum.php">انجمن</li></a>
-            <li><a href="Courses.php" class="btn-start-learning">شروع یادگیری</li></a>
+            <li><a href="index.php" class="btn" style="transition: 119ms;"> خانه</a></li>
+            <li><a href="AboutUs.php" class="btn" style="transition: 119ms;">درباره ما</a></li>
+            <li><a href="ContactUs.php" class="btn" style="transition: 119ms;">تماس با ما</a></li>
+            <li><a href="forum/controllers/Forum.php" class="btn" style="transition: 119ms;">انجمن</a></li>
         </ul>
     </nav>
-</header>-->
-<header class="masthead">
-    <p class="masthead-intro"><a href="index.php"><img src="assets/images/logo-index.png" alt="logo" width="7%" height="auto"></a></p>
+</header>
+<div class="masthead">
+    <p class="masthead-intro"><a href="index.php"><img src="assets/images/pink-logo.png" alt="logo"></a></p>
     <h1 class="masthead-heading">fastscroll.com</h1>
     <hr class="header-divider">
-</header>
-<!--Our service section -->
-<br>
-<br>
-<br>
-<br>
-
+</div>
+<!--End Header-->
+<!--Start Our service section -->
 <div class="container">
 <div class="row">
-    <div class="col-md-6">
-        <section class="questions-section img-thumbnail" dir="rtl">
-            <h2>هدف کلی وبسایت</h2>
-            <p>درآمد بالای برنامه‌نویسان، علاقه به کامپیوتر و مباحث پیشرفته‌ی آن، وجهه‌ی شخصیتی که برنامه‌نویسان در اجتماع دارند، نیاز به شغلی که بتوانید در خانه بمانید و پول در بیاورید، تغییر جهان یا حتی به فعلیت رساندن ایده‌هایتان.<br> این‌ها همه دلایل بسیار خوبی برای شروع یادگیری برنامه‌نویسی هستند.ولی آنچه مهم هست پیدا کردن مسیر و طی کردن آن می باشد.براستی چطور و از کجا باید برنامه‌نویسی را شروع کنیم؟
-                اگر می خواهید یک نقشه راه خوب و مطمن برای یادگیری برنامه‌نویسی و ادامه‌ی کار در این حوزه داشته باشید،...،با مطالب و منابع جامع خود کار را برای شما آسان کرده است تا در وقت و انرژی‌تان صرفه‌جویی و مراحل یادگیری را بهتر و سریع‌تر طی کنید و در ادامه کار برای حرفه ای شدن با شما خواهد بود تا پس از آن بتوانید به بازار کار ورود پیدا کنید تا آینده کاری خود را تضمین نمایید.</p>
-            <p> سایت آموزش برنامه نویسی ... به دانشجویان و برنامه نویسان کمک می کند که برنامه نویسی را اصولی و رایگان بیاموزند و دیگر دغدغه ی آموزشی و نرم افزاری نداشته باشند .</p>
-        </section>
-    </div>
-    <div class="col-md-6">
-        <section class="questions-section img-thumbnail" dir="rtl">
-            <h2>خدماتی که ما ارائه می کنیم:</h2>
-            <p class="our-service">
-            <li>ارائه مطالب آموزشی رایگان ( فیلم آموزشی فارسی و زبان اصلی ، کتاب و جزوه آموزشی ، نرم افزار و کامپوننت ، سورس و پروژه )</li>
-            <li>ارائه پکیج های آموزشی با کیفیت عالی و قیمت مناسب</li>
-            <li>برگزاری دوره های برنامه نویسی ، گرافیک ، شبکه ، بازاریابی ، کسب و کار و . . . به صورت مجازی</li>
-            <li>برگزاری دوره های برنامه نویسی ، گرافیک ، شبکه ، بازاریابی ، کسب و کار و . . . به صورت مجازی</li>
-            ما در این سایت جدیدترین و کاربردی ترین علم روز در زمینه برنامه نوسی را در اختیار شما قرار خواهیم داد .
-            </p>
-        </section>
-    </div>
-</div>
-    <hr>
-</div>
-
+    <div class="col-md-12">
+        <section class="about-section img-thumbnail" dir="rtl">
+            <p>
+    درآمد بالای برنامه‌نویسان، علاقه به کامپیوتر و مباحث پیشرفته‌ی آن، وجهه‌ی شخصیتی که برنامه‌نویسان در اجتماع دارند، نیاز به شغلی که بتوانید در خانه بمانید و پول در بیاورید،
+    تغییر جهان یا حتی به فعلیت رساندن ایده‌هایتان. این‌ها همه دلایل بسیار خوبی برای شروع یادگیری برنامه‌نویسی هستند.
+    ولی آنچه مهم هست پیدا کردن مسیر و طی کردن آن می باشد.براستی چطور و از کجا باید برنامه‌نویسی را شروع کنیم؟
+</p>
+<p>
+    اگر می خواهید یک نقشه راه خوب و مطمن برای یادگیری برنامه‌نویسی و ادامه‌ی کار در این حوزه داشته باشید، fastscroll ،با مطالب و منابع جامع خود کار را برای شما آسان کرده است
+    تا در وقت و انرژی‌تان صرفه‌جویی و مراحل یادگیری را بهتر و سریع‌تر طی کنید و در ادامه کار برای حرفه ای شدن با شما خواهد بود
+    تا پس از آن بتوانید به بازار کار ورود پیدا کنید تا آینده کاری خود را تضمین نمایید.
+</p>
+<p>
+    سایت fastscroll در پاییز سال 1399 هجری شمسی با هدف ایجاد یک بانک آموزشی رایگان برنامه نویسی تشکیل شده و با عنوان سایت (FASTSCROLL.COM) شروع به فعالیت نمود .
+    سایت آموزش برنامه نویسی fastscroll به دانشجویان و برنامه نویسان کمک می کند که برنامه نویسی را اصولی و رایگان بیاموزند و دیگر دغدغه ی آموزشی و نرم افزاری نداشته باشند .
+</p></section></div></div></div>
+<!--End Our service section -->
 <!--- Start footer-->
 <div id="footer">
     <div class="footerContent wrapper">
         <div class="socialCounts row social-img-group align-items-center align-content-center justify-content-center">
             <a class="" href="https://twitter.com/FastScroll/" target="_blank">
-                <img src="assets/images/Social-img/twitter.png" class="social-img" title="twitter">
+                <img src="assets/images/Social-img/white-twitter.png" class="social-img " title="twitter">
             </a>
             <a class="align-items-center" href="http://www.facebook.com/FastScroll/" target="_blank">
-                <img src="assets/images/Social-img/facebook.png" class="social-img" title="facebook">
+                <img src="assets/images/Social-img/white-facebook.png" class="social-img" title="facebook">
             </a>
             <a class="" href="http://www.github.com/FastScroll/" target="_blank">
-                <img src="assets/images/Social-img/github.png" class="social-img" title="Github">
+                <img src="assets/images/Social-img/white-github.png" class="social-img" title="Github">
             </a>
             <a class="" href="http://www.linkdin.com/FastScroll/" target="_blank">
-                <img src="assets/images/Social-img/linkedin.png" class="social-img" title="Linkdin">
+                <img src="assets/images/Social-img/white-linkedin.png" class="social-img" title="Linkdin">
             </a>
         </div>
-
-    <a href="#fastscroll" class="anchor" title="بالا"><img src="assets/images/sort-up-64.png" width="5%" alt="بالا"></a>
+        <a href="#fastscroll" class="anchor" title="بالا"><img src="assets/images/white-scroll-up.png"  alt="بالا"></a>
+    </div>
     <div class="copyright" dir="ltr">
         © 2020 FastScroll, Inc. All rights reserved.
     </div>
