c15df1a6c4b68fb5043a552bda2f24eba865ebcd
Zმհɾმ Rმzzმghi <47444155+ZahraRazzaghi@users.noreply.github.com>
2020-09-10 17:06:26 +0430

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,92 @@
+<!--Start footer-->
+<div id="footer">
+    <div class="container footer-link-group text-center center bg-dark">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>توسعه وب</h4>
+                <ul>
+                    <li><a href="Course/controllers/HTML.php">HTML</a></li>
+                    <li><a href="Course/controllers/CSS.php">CSS</a></li>
+                    <li><a href="Course/controllers/JavaScript.php">JavaScript</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>سمت سرور</h4>
+                <ul>
+                    <li><a href="Course/controllers/PHP.php">PHP</a></li>
+                    <li><a href="Course/controllers/SQL.php">SQL</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>ابزارها</h4>
+                <ul>
+                    <li><a href="Code-Editor/Code-Editor.php">Code Editor</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footerContent">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 align-content-center text-center row ">
+                <div class="col-md-8">
+                    <h5 dir="ltr" class="contentTitle">ویژگی ها</h5>
+                    <ul class="tutorial-groups text-light">
+
+                        <li><a class="">در وب و در حال حرکت بیاموزید.</a></li>
+                        <li><a class="">در همه دستگاه ها و سیستم عامل های اصلی موجود است.</a></li>
+                        <li><a class="">ساده تر و لذت بخش تر از همیشه!</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5 dir="ltr" class="contentTitle">خدمات</h5>
+                    <ul class="tutorial-groups text-light">
+
+                        <li><a class="">طراحی و توسعه وب</a></li>
+                        <li><a class="">طراحی و توسعه برنامه های مبایل</a></li>
+                        <li><a class="">آموزش های ویدئویی</a></li>
+                        <li><a class="">پلتفرم یادگیری آنلاین</a></li>
+                        <li><a class="">مسیرهای یادگیری</a></li>
+                        <li><a class="">گواهی نامه های آنلاین</a></li>
+                        <li><a class="">مقالات بروز</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 align-content-center center">
+                <div class="footerMenu Courses">
+                    <ul>
+                        <li><a href="ContentManagement/Articles/index.php" id="footerHome">خانه</a></li>
+                        <li><a href="AboutUs.php">درباره ما</a></li>
+                        <li><a href="ContactUs.php" id="footerCourses">ارتباط با ما</a></li>
+                        <li><a href="#last-tutorials" class="">آخرین دوره ها</a></li>
+                        <li><a href="#last-articles" class="">آخرین مقالات</a></li>
+                        <li><a href="Course/controllers/Courses.php" id="footerContacts">آموزش ها</a></li>
+                        <li><a href="CommonQuestions.php" id="faq" >سؤالات متداول</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class=" align-content-center center bg-dark" style="padding: 10px">
+                <p style="color:#fafafa; font-weight: 100">FastScroll را در شبکه های اجتماعی دنبال کنید</p>
+                <hr class="bg-hr">
+                <div class="socialCounts row align-content-center center">
+                    <a  href="https://twitter.com/FastScroll/" target="_blank" title="twitter">
+                        <div ><img src="assets/images/Social-img/twitter.png" title="" width="59%"></div>
+                    </a>
+                    <a  href="http://www.facebook.com/FastScroll/" target="_blank">
+                        <div ><img src="assets/images/Social-img/facebook.png" title="facebook" width="59%"></div>
+                    </a>
+                    <a  href="http://www.github.com/FastScroll/" target="_blank">
+                        <div ><img src="assets/images/Social-img/github.png" title="Github" width="59%"></div>
+                    </a>
+                    <a  href="http://www.linkdin.com/FastScroll/" target="_blank">
+                        <div><img src="assets/images/Social-img/linkedin.png" title="Linkdin" width="59%"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#fastscroll" class="anchor" title="بالا"><img src="assets/images/white-scroll-up.png" width="50pt" alt="بالا"></a>
+    <div class="copyright" dir="ltr">
+        © 2020 FastScroll, Inc. All rights reserved.
+    </div>
+</div>
+<!---End footer --->
