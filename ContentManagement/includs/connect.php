0d964931d79d4fa0a830bf45abd622122c477877
Zმհɾმ Rმzzმghi <47444155+ZahraRazzaghi@users.noreply.github.com>
2020-09-15 16:54:09 +0430

    Delete connect.php

--- ContentManagement/includs/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $con=mysqli_connect('localhost','root','','content_management') or die(mysql_error());
-    mysqli_set_charset($con, "utf8");
-
-}catch(PDOException $error){
-    echo 'Error' . $error->getMessage();
-}
-?>
